52921e56b18b1393c3e4fca628a940098bffcbd6
Chales Horn <charles.horn@gmail.com>
2023-05-11 09:28:26 +1200

    introduce SVG bounds method

--- lithomancy/svgout.py
+++ lithomancy/svgout.py
@@ -16,6 +16,7 @@ def arrow():
                 fill_opacity=0.3
          )])
 
+
 def dot():
     return svg.Marker(
         id='dot',
@@ -31,28 +32,35 @@ def dot():
          )])
 
 
+def bounds(sticks: list[Stick]) -> tuple:
+    WIDTH = 500
+    HEIGHT = 500
+
+    # returns width, height, offset
+    return WIDTH, HEIGHT, (WIDTH // 2, HEIGHT // 2)
+
+
 def output(sticks: list[Stick], arrows: bool = False) -> svg.SVG:
     """
     Generates an SVG representation of the sticks source code.
     The "beach" on which the sticks are laid out.
     """
     COLORS = {'a': 'magenta', 'b': 'blue', 'c': 'yellow', 'g': 'grey'}
-    WIDTH = 500
-    HEIGHT = 500
     ppu = 10
-    beach = svg.SVG(width=WIDTH, height=HEIGHT)
+    w, h, offset = bounds(sticks)
+    beach = svg.SVG(width=w, height=h)
     elements = []
     marker = arrow() if arrows else dot()
     defs = svg.Defs(
         elements=[marker]
     )
-    elements = [defs, svg.Circle(cx=WIDTH//2, cy=HEIGHT//2, r=1, stroke='red')]
+    elements = [defs, svg.Circle(cx=offset[0], cy=offset[1], r=1, stroke='red')]
 
     for s in sticks:
         elements.append(
            svg.Line(
-                x1=ppu * s.a[0] + WIDTH // 2, x2=ppu * s.b[0] + WIDTH // 2,
-                y1=ppu * s.a[1] + HEIGHT // 2, y2=ppu * s.b[1] + HEIGHT // 2,
+                x1=ppu * s.a[0] + offset[0], x2=ppu * s.b[0] + offset[0],
+                y1=ppu * s.a[1] + offset[1], y2=ppu * s.b[1] + offset[1],
                 stroke=COLORS[s.material],
                 stroke_width=1,
                 marker_end=f'url(#{marker.id})',
